288e99dc2c64deff2bce153b4ea03b6cae5f3445
moumkinehhazar <moumkine.hazar@gmail.com>
2023-11-29 00:51:31 +0100

    push

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# Author - Tolulope Fakunle
+# Author - hajar moumkine
 
 def islower(c):
     """Function checks for lowercase characters."""
--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# Author - Tolulope Fakunle
+# Author - hajar moumkine
 
 def uppercase(str):
     """Print a string in uppercase."""
