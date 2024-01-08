aeab53fb630a1f3f8279975d9bd27c5e85564418
moumkinehhazar <moumkine.hazar@gmail.com>
2024-01-08 16:23:54 +0100

    push

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,10 +1,18 @@
 #!/usr/bin/python3
-"""Defines an inherited list class MyList."""
+"""
+Contains the class BaseGeometry
+"""
 
 
-class MyList(list):
-    """Implements sorted printing for the built-in list class."""
+class BaseGeometry:
+    """A class with public instance methods area and integer_validator"""
+    def area(self):
+        """raises an exception when called"""
+        raise Exception("area() is not implemented")
 
-    def print_sorted(self):
-        """Print a list in sorted ascending order."""
-        print(sorted(self))
+    def integer_validator(self, name, value):
+        """validates that value is an integer greater than 0"""
+        if type(value) is not int:
+            raise TypeError("{:s} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{:s} must be greater than 0".format(name))
